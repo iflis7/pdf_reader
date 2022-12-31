f9476788a60e5866812b23c084213c806c1387ee
bylkode <hafidsaadibk@gmail.com>
2022-12-30 19:34:57 -0500

    code added but PyPDF2 getnumpages method deprecated

--- src/main.py
+++ src/main.py
@@ -1,23 +1,40 @@
-# Import necessary libraries:
 import pyttsx3
 import PyPDF2
 
-# Initialize the text-to-speech engine using "pyttsx3".
-engine = pyttsx3.init('dummy')
+# Read the file in binary mode:
+book = open('books/HAFID-SAADI_CV_.pdf', 'rb')
 
-# Open the PDF file using "PyPDF2".
-book = open('books/think_and_grow_rich.pdf', 'rb')
-with open('books/think_and_grow_rich.pdf', 'rb') as f:
-    pdf_reader = PyPDF2.PdfReader(f)
+# Create a PdfFileReader object:
+pdfReader = PyPDF2.PdfReader(book)
 
-# Extract the text from each page of the PDF using the getPage()
-# and extractText() methods of the PdfFileReader object.
-pages = []
-for page in range(len(pdf_reader.pages)):
-    pages.append(pdf_reader.getPage(page).extractText())
+# To determine total number of pages in the PDF file:
+pages = len(pdfReader.pages)
 
-# Use the say() method of the pyttsx3 engine to read each page of the PDF out loud.
+# Initialize the speaker:
+# Here, init() function is used to get a reference to a pyttsx3.Engine instance
+speaker = pyttsx3.init()
 
-for page in pages:
-    engine.say(page)
-engine.runAndWait()
+# To access voice property of the speaker:
+voices = speaker.getProperty('voices')
+
+# Set the speaker's gender: 0-> Male (default), 1-> Female
+speaker.setProperty('voice', voices[1].id)
+
+# Iterate through the pages you want to access
+# For accessing specific pages: Iterate through the corresponding page indices
+# Note: Index of first page-> 0
+# Here, entire PDF is accessed:
+for num in range(pages):
+    # To read current page index:
+    page = pdfReader.getPage(num)
+    # To extract the text present in current page:
+    text = page.extractText()
+    # say() function takes a string as the parameter and then queues the same to be converted from text-to-speech
+    speaker.say(text)
+    # runAndWait() function blocks the engine instance until all the currently queued commands are processed
+    speaker.runAndWait()
+
+# To save the audio output as a MP3 file, within this project:
+# Make use of any MP3 player to access this recording whenever required
+speaker.save_to_file(text, 'audio.mp3')
+speaker.runAndWait()
